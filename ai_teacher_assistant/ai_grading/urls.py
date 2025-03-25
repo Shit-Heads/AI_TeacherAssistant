2bc5817af4f42cb20caba3a3cc2ad485659ca234
Anjana-1403 <anjananarayanan1403@gmail.com>
2025-03-25 14:27:31 +0530

    updated submissions

--- ai_teacher_assistant/ai_grading/urls.py
+++ ai_teacher_assistant/ai_grading/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     #path('', views.chat_view, name='chat'),  # Render chat.html at /ai/
     #path('ask', views.ask, name='ask'),  # POST request handling for AI interaction
-    path('upload/', views.upload_view, name='upload_view'),  
+    path('', views.upload_view, name='upload_view'),  
     path('process_json/', views.process_json, name='process_json'), 
 ]
