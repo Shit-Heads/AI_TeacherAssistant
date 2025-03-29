add70e450ef7336cd66076efb885a7263baf9fb7
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2025-03-29 12:25:43 +0530

    ai grading corrected

--- ai_teacher_assistant/ai_grading/urls.py
+++ ai_teacher_assistant/ai_grading/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("submissions_view/", views.submissions_view, name="submissions_view"),
     path("submissions_view/get-submissions/", views.get_submissions, name="get_submissions"),
     path("submissions_view/upload-submission/", views.upload_submission, name="upload_submission"),
-    path('submissions_view/get-ai-grading/<str:ai_assessments_id>/', views.get_ai_grading, name='get_ai_grading'),
+    path('submissions_view/get-ai-grading/<str:submission_id>/', views.get_ai_grading, name='get_ai_grading'),
 ]
  
